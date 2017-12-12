bf92ed311eeedb1b245754a0f207cb77bae97d6a
Cowbon <alexalex850520@gmail.com>
2017-12-12 16:52:51 +0800

    add 403 forbidden, add admin permission control, add the limit of contact selected

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_login import LoginManager
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
+# Brootstrap, DebugToolbar, SQLAlchemy
 Bootstrap(app)
 DebugToolbarExtension(app)
-
 db = SQLAlchemy(app)
 
 # login-manage
+from flask_login import LoginManager
 from .models import User
 
 login_manager = LoginManager()
@@ -24,5 +24,20 @@ login_manager.login_view = 'login'
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
+# admin
+from flask import redirect, url_for, make_response, render_template
+from flask_login import current_user
+from functools import wraps
+
+def admin_only(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if current_user.username == "admin":
+			return f(*args, **kwargs)
+		else:
+			return make_response(render_template('403_forbidden.html', current_user=current_user, message="You are not the ADMIN!"), 403)
+	return decorated_function
+
 # views
 from .view import *
+
