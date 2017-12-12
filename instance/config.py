70c7b9ff121c4140cb1399cb0cd0626a195e67f2
Kevin Chen <me@kevchentw.com>
2017-12-12 19:00:57 +0800

    update db

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'im_msg_web_secret_key'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:whomatter@0.0.0.0:3306/who_matter2'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:whomatter@who.nctu.me:3306/who_matter2'
