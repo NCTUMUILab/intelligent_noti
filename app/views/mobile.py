9b8197caff24f97f473d08b9b96770f99654fa60
Kevin Chen <me@kevchentw.com>
2017-12-24 22:19:41 +0800

    update

--- app/views/mobile.py
+++ app/views/mobile.py
@@ -13,6 +13,48 @@ import dateutil.parser
 
 mobile = Blueprint('mobile', __name__)
 
+def get_notification_data(user):
+    delta = datetime.utcnow() - timedelta(minutes=30)
+    u = db.session.query(User).filter(User.phone_id==user).one_or_none()
+    notification = db.session.query(Result).filter(Result.date > delta).filter(Result.r_type == 'Notification').filter(Result.raw.like('com.facebook.orca%')).filter(Result.user == user).order_by(desc(Result.created_at)).all()
+    db.session.close()
+    notifications = []
+    wids = []
+    for n in notification:
+        noti = n.raw.split('\t')
+        p = {}
+        if noti[0] == 'com.facebook.orca':
+            p['app'] = 'Facebook'
+            p['title'] = noti[1]
+            p['content'] = noti[2]
+            p['time'] = n.date + timedelta(hours=8)
+            p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name==p['title']).one_or_none()
+            if not p['white_list_user']:
+                continue
+            wids.append(p['white_list_user'].id)
+            p['wid'] = p['white_list_user'].id
+            p['hash'] = hashlib.md5((str(noti[0:2]) + str(n.date)).encode()).hexdigest()
+            p['done'] = bool(db.session.query(FormResult).filter(FormResult.hash==p['hash']).one_or_none())
+            notifications.append(p)
+    notifications = reversed(sorted(notifications, key=lambda k: k['time']))
+    return list(notifications)
+
+def get_form_valid(notifications, user):
+    contacts = []
+    for n in notifications:
+        contacts.append(n['title'])
+    contacts = list(set(contacts))
+    last_form = db.session.query(FormResult).filter(FormResult.user==user).order_by(desc(FormResult.created_at)).first()
+    if last_form:
+        last_form_time = last_form.created_at
+        last_form_sender = last_form.sender
+    now = datetime.now()
+    hour = now.hour
+    delta_last_form =  now - last_form_time
+    if (hour >= 8 and hour <= 22) and (delta_last_form > timedelta(minutes=90)) and (last_form_sender not in contacts):
+        return True
+    return False
+
 @mobile.route('/form/', methods=['POST'])
 def add_form():
     content = request.get_json(silent=True)
@@ -26,7 +68,7 @@ def add_form():
          db.session.rollback()
          msg = 'error'
     return jsonify({'msg': msg})
-    
+
 
 @mobile.route('/upload/', methods=['POST'])
 def add_result():
@@ -45,49 +87,22 @@ def add_result():
             msg = 'error'
     if content:
         user = content[0]['user']
-    result = db.session.query(FormResult).filter(FormResult.user).order_by(desc(FormResult.created_at)).first()
-    if result:
-        result = result.created_at
-    print({'msg': 'ok', 'last_form': result})
-    return jsonify({'msg': 'ok', 'last_form': result})
+    last_form = db.session.query(FormResult).filter(FormResult.user==user).order_by(desc(FormResult.created_at)).first()
+    if last_form:
+        last_form = last_form.created_at
+
+    notifications = get_notification_data(user)
+    form_valid = get_form_valid(notifications, user)
+
+    return jsonify({'msg': 'ok', 'last_form': last_form, 'form_valid': form_valid})
 
 
 @mobile.route('/notification/')
 def get_notification():
-    delta = datetime.utcnow() - timedelta(minutes=600)
     user = request.args.get('user')
-    u = db.session.query(User).filter(User.phone_id==user).one_or_none()
-    notification = db.session.query(Result).filter(Result.r_type == 'Notification').filter(Result.user == user).filter(Result.date > delta).order_by(desc(Result.created_at)).all()
-    db.session.close()
-    notifications = []
-    wids = []
-    for n in notification:
-        noti = n.raw.split('\t')
-        p = {}
-        if noti[0] == 'com.facebook.orca':
-            p['app'] = 'Facebook'
-            p['title'] = noti[1]
-            p['content'] = noti[2]
-            p['time'] = n.date
-            p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name==p['title']).one_or_none()
 
-            if not p['white_list_user']:
-                continue
-            wids.append(p['white_list_user'].id)
-            p['wid'] = p['white_list_user'].id
-            p['hash'] = hashlib.md5((str(noti[0:2]) + str(n.date)).encode()).hexdigest()
-            p['done'] = bool(db.session.query(FormResult).filter(FormResult.hash==p['hash']).one_or_none())
-            notifications.append(p)
-    notifications = sorted(notifications, key=lambda k: k['time'])
-    wid_count = {}
-    for wid in wids:
-        wid_count[wid] = db.session.query(FormResult).filter(FormResult.wid==wid).count()
-    _wid_count = wid_count
-    wid_count = sorted(wid_count, key=wid_count.get)
-    selected = []
-    for w in wid_count[0:3]:
-        for n in notifications:
-            if n['wid'] == w:
-                selected.append(n)
-                break
-    return render_template('notification.html', notification=selected, wid_count=_wid_count)
+    notifications = get_notification_data(user)
+    form_valid = get_form_valid(notifications, user)
+
+    print(form_valid)
+    return render_template('notification.html', notification=notifications)
