a528aa402ff393f67425b13e64f59d5678bbc4f3
Ubuntu <ubuntu@ip-172-31-32-21.us-east-2.compute.internal>
2018-01-01 20:18:29 +0800

    update

--- app/views/mobile.py
+++ app/views/mobile.py
@@ -14,7 +14,7 @@ import dateutil.parser
 mobile = Blueprint('mobile', __name__)
 
 def get_notification_data(user):
-    delta = datetime.utcnow() - timedelta(minutes=30)
+    delta = datetime.utcnow() - timedelta(minutes=5)
     u = db.session.query(User).filter(User.phone_id==user).one_or_none()
 
     notification_line = db.session.query(Result).filter(Result.date > delta).filter(Result.r_type == 'Notification').filter(Result.raw.like('jp.naver.line.android%')).filter(Result.user == user).order_by(desc(Result.created_at)).all()
@@ -30,11 +30,14 @@ def get_notification_data(user):
             p['title'] = noti[1]
             p['content'] = noti[2]
             p['time'] = n.date + timedelta(hours=8)
-            p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name==p['title']).one_or_none()
-            if not p['white_list_user']:
+            try:
+                p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name==p['title']).one_or_none()
+            except Exception:
+                p['white_list_user'] = None
+            if not p['white_list_user'] and (not user == "c891e5cda613e2ac"):
                 continue
-            wids.append(p['white_list_user'].id)
-            p['wid'] = p['white_list_user'].id
+    #        wids.append(p['white_list_user'].id)
+    #        p['wid'] = p['white_list_user'].id
             p['hash'] = hashlib.md5((str(noti[0:2]) + str(n.date)).encode()).hexdigest()
             p['done'] = bool(db.session.query(FormResult).filter(FormResult.hash==p['hash']).one_or_none())
             notifications.append(p)
@@ -46,11 +49,14 @@ def get_notification_data(user):
             p['title'] = noti[1]
             p['content'] = noti[2]
             p['time'] = n.date + timedelta(hours=8)
-            p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name_line==p['title']).one_or_none()
-            if not p['white_list_user']:
+            try:
+                p['white_list_user'] = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id).filter(ContactQuestionnaire.contact_name_line==p['title']).one_or_none()
+            except Exception:
+                p['white_list_user'] = None
+            if not p['white_list_user'] and (not user == "c891e5cda613e2ac"):
                 continue
-            wids.append(p['white_list_user'].id)
-            p['wid'] = p['white_list_user'].id
+    #        wids.append(p['white_list_user'].id)
+            p['wid'] = 0
             p['hash'] = hashlib.md5((str(noti[0:2]) + str(n.date)).encode()).hexdigest()
             p['done'] = bool(db.session.query(FormResult).filter(FormResult.hash==p['hash']).one_or_none())
             notifications.append(p)
@@ -63,22 +69,20 @@ def get_form_valid(notifications, user):
         contacts.append(n['title'])
     contacts = list(set(contacts))
     last_form = db.session.query(FormResult).filter(FormResult.user==user).order_by(desc(FormResult.created_at)).first()
+    now = datetime.now()
+    hour = now.hour
     if last_form:
         last_form_time = last_form.created_at
         last_form_sender = last_form.sender
-    elif contacts:
+    elif (hour >= 8 and hour <= 22) and contacts:
         return True
     else:
         return False
 
-    now = datetime.now()
-    hour = now.hour
     delta_last_form =  now - last_form_time
-    if (hour >= 8 and hour <= 22) and (delta_last_form > timedelta(minutes=180)) and contacts:
-        return True
-    if last_form_sender in contacts:
-        contacts.remove(last_form_sender)
-    if (hour >= 8 and hour <= 22) and (delta_last_form > timedelta(minutes=90)) and contacts:
+    #if last_form_sender in contacts:
+    #    contacts.remove(last_form_sender)
+    if (hour >= 8 and hour <= 22) and (delta_last_form > timedelta(minutes=60)) and contacts:
         return True
 
     return False
@@ -124,16 +128,22 @@ def add_result():
 
     return jsonify({'msg': 'ok', 'last_form': last_form, 'form_valid': form_valid})
 
+@mobile.route('/whitelist/')
+def whitelist():
+    user = request.args.get('user')
+    u = db.session.query(User).filter(User.phone_id==user).one_or_none()
+    contactQuestionnaire = db.session.query(ContactQuestionnaire).filter(ContactQuestionnaire.user_id==u.id)
+    result = ''
+    for c in contactQuestionnaire:
+        if c.contact_name:
+            result += '(line) ' + c.contact_name + '\t'
+        if c.contact_name_line:
+            result += '(fb) ' + c.contact_name_line + '\t'
+    return jsonify({'result': result})
+
 
 @mobile.route('/notification/')
 def get_notification():
-    user = request.args.get('user')
+    data = request.args.get('data')
 
-    notifications = get_notification_data(user)
-    form_valid = get_form_valid(notifications, user)
-    if len(notifications) > 10:
-        notifications = notifications[0:10]
-    msg = ""
-    if len(notifications) == 0:
-        msg = '現在沒有問卷喔!'
-    return render_template('notification.html', notification=notifications, msg=msg, count=len(notifications))
+    return render_template('notification.html', data=data)
