fe2323add7c4a5f621a6995348fe280ffc7cc710
Hank's Mac <hankhplee@gmail.com>
2018-03-15 18:37:18 +0800

    update qualtric to db

--- script/qualtrics_to_db.py
+++ script/qualtrics_to_db.py
@@ -7,8 +7,8 @@ db = MySQLdb.connect("ec2-18-220-229-235.us-east-2.compute.amazonaws.com","root"
 cursor = db.cursor()
 
 add_data = ("INSERT INTO esm_data"
-               "(StartDate, EndDate, Status, IPAddress, Progress, Duration, Finished, RecordedDate, ResponseId, RecipientLastName,RecipientFirstName, RecipientEmail,ExternalReference, LocationLatitude, LocationLongitude, DistributionChannel, UserLanguage, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13,Q14,Q15,Q16,Q17,Q18,app,title,text,created_at,user,time,test,Q5Topics,textTopics) "
-               "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+               "(StartDate, EndDate, Status, IPAddress, Progress, Duration, Finished, RecordedDate, ResponseId, RecipientLastName,RecipientFirstName, RecipientEmail,ExternalReference, LocationLatitude, LocationLongitude, DistributionChannel, UserLanguage, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13,Q14,Q15,Q16,Q17,Q18,Q19,app,title,text,created_at,user,time,test,Q5Topics,textTopics) "
+               "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
 add_data2 = ("INSERT INTO esm_data"
                "(StartDate, EndDate) "
                "VALUES (%s,%s)")
@@ -22,7 +22,8 @@ with open(filename, 'r') as f:
             if(count<3):
                 count = count + 1
                 continue
-            data=(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23],row[24],row[25],row[26],row[27],row[28],row[29],row[30],row[31],row[32],row[33],row[34],row[35],html.unescape(row[36]),html.unescape(row[37]),row[38],row[39],row[40],row[41],row[42],row[43])
+            data=(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23],row[24],row[25],row[26],row[27],row[28],row[29],row[30],row[31],row[32],row[33],row[34],row[35],row[36],html.unescape(row[37]),html.unescape(row[38]),row[39],row[40],row[41],row[42],row[43],row[44])
+            print(data)
             """
             data = {
             'StartDate': row[0],
@@ -60,19 +61,20 @@ with open(filename, 'r') as f:
             'Q16': row[32],
             'Q17': row[33],
             'Q18': row[34],
-            'app': row[35],
-            'title': row[36],
-            'text': row[37],
-            'created_at': row[38],
-            'user': row[39],
-            'time': row[40],
-            -'test': row[41],
-            'Q5Topics': row[42],
-            'textTopics': row[43]
+            'Q19': row[35]
+            'app': row[36],
+            'title': row[37],
+            'text': row[38],
+            'created_at': row[39],
+            'user': row[40],
+            'time': row[41],
+            -'test': row[42],
+            'Q5Topics': row[43],
+            'textTopics': row[44]
             }
             StartDate, EndDate, Status, IPAddress, Progress, Duration, Finished, RecordedDate, ResponseId, RecipientLastName,RecipientFirstName, RecipientEmail, ExternalReference,ExternalReference, LocationLatitude, LocationLongitude, DistributionChannel, UserLanguage, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13,Q14,Q15,Q16,Q17,Q18,app,title,text,created_at,user,time,test,Q5Topics,textTopics
             """
-            #print(html.unescape(row[36]))
+
             cursor.execute(add_data, data)
 
     except csv.Error as e:
